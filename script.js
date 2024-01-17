973291c4b767454b00553c7fe93a3805213a65fb
RomulloOliveira <felipe2006__@hotmail.com>
2024-01-17 20:55:55 -0300

    Alterações de valores

---
+++ script.js
@@ -0,0 +1,8 @@
+
+const convertButtom = document.querySelector (".convert-button")
+
+function convertValues(){
+    console.log("Funcionou?")
+}
+
+convertButtom.addEventListener("click", convertValues)
