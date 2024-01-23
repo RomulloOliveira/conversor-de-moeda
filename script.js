ea8aa47bbc6c33825416b3032cfdd5f686d53171
RomulloOliveira <felipe2006__@hotmail.com>
2024-01-22 21:43:12 -0300

    desenvolvendo logica

--- script.js
+++ script.js
@@ -2,7 +2,14 @@
 const convertButtom = document.querySelector (".convert-button")
 
 function convertValues(){
-    console.log("Funcionou?")
+   
+    const inputCurrencyValue = document.querySelector(".input-currency").value
+    
+    const dolarToday = 4.99
+    
+    const convertedValue = inputCurrencyValue / dolarToday
+
+    console.log(convertedValue)
 }
 
 convertButtom.addEventListener("click", convertValues)
